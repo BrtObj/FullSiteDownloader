cb36bc047cd19a0e51e51a6fbc1222cfb823572b
brtobj <BrtObj@hotmail.com>
2017-07-12 22:15:16 +0900

    Add project files.

---
+++ FullSiteDownloader/DBUtil/DBAccess.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullSiteDownloader.DBUtil
+{
+    class DBAccess
+    {
+        static string MsSqlConnectionString = ConfigurationManager.ConnectionStrings["MsSql"].ConnectionString;
+
+        private SqlConnection CreateMsSqlConnection()
+        {
+            return new SqlConnection(MsSqlConnectionString);
+        }
+
+        public string test()
+        {
+            string _result = "";
+            SqlConnection _connection = null;
+            try
+            {
+                _connection = this.CreateMsSqlConnection();
+                _connection.Open();
+                _result = string.Format("数据库版本: {0}\n" +
+                    "状态: {1}", _connection.ServerVersion, _connection.State);
+            }
+            catch (Exception ex)
+            {
+                _result = ex.Message;
+            }
+            finally
+            {
+                if (_connection != null)
+                    _connection.Close();
+            }
+            return _result;
+        }
+
+        public DataTable ExecSql(string sqlText)
+        {
+            DataTable _dt = new DataTable();
+            SqlConnection _connection = null;
+            try
+            {
+                _connection = this.CreateMsSqlConnection();
+                _connection.Open();
+                SqlDataAdapter _adapter = new SqlDataAdapter(sqlText, _connection);
+                _adapter.Fill(_dt);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return _dt;
+        }
+
+        public int ExecNonQuery(string sqlText)
+        {
+            int _result = -1;
+            SqlConnection _connection = null;
+            try
+            {
+                _connection.Open();
+                SqlCommand _command = new SqlCommand(sqlText, _connection);
+                _result = _command.ExecuteNonQuery();
+            }
+            catch (Exception) {
+                throw;
+            }
+            finally {
+                _connection.Close();
+            }
+            return _result;
+        }
+    }
+}
---
+++ FullSiteDownloader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FullSiteDownloader.DBUtil;
+using System.Data;
+
+//todo:
+// url解析并逐级生成文件夹
+// 数据库封装，支持mssql mysql oracle
+// downloadUtil 。。。
+// 网站快速遍历，下载更新
+
+namespace FullSiteDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBAccess _dbAccess = new DBAccess();
+            string _result = _dbAccess.test();
+            Console.WriteLine(_result);
+            DataTable _dbs = _dbAccess.ExecSql("select name from SysDatabases");
+            Console.Write("库: ");
+            foreach(DataRow _row in _dbs.Rows)
+            {
+                Console.Write(_row.ItemArray[0] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
